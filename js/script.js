677bc12e30dc1b78d60117fa4a2042d162b9ca21
DavidZarfati <zarda2001@gmail.com>
2025-11-07 17:00:58 +0100

    feat:aggiunta controllo numeri univoci

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 let numeriGenerati = 0
 let numeroGenerato = 0
 let arrayNumeri = []
-let numeroUno = document.querySelector(".numeri h5:nth-child(1)")
-let numeroDue = document.querySelector(".numeri h5:nth-child(2)")
-let numeroTre = document.querySelector(".numeri h5:nth-child(3)")
-let numeroQuattro = document.querySelector(".numeri h5:nth-child(4)")
-let numeroCinque = document.querySelector(".numeri h5:nth-child(5)")
+// let numeroUno = document.querySelector(".numeri h5:nth-child(1)")
+// let numeroDue = document.querySelector(".numeri h5:nth-child(2)")
+// let numeroTre = document.querySelector(".numeri h5:nth-child(3)")
+// let numeroQuattro = document.querySelector(".numeri h5:nth-child(4)")
+// let numeroCinque = document.querySelector(".numeri h5:nth-child(5)")
 let bottoneStart = document.querySelector(".bg-primary")
 let bottoneSubmit = document.querySelector(".bg-secondary")
+
+
+let numbers = document.querySelectorAll('.numeri h5');
 
 
 
@@ -20,34 +23,46 @@ bottoneSubmit.addEventListener("click", confrontoNumeri)
 bottoneSubmit.disabled = true
 
 function generatoreNumeri() {
-    bottoneStart.disabled = true
 
-    while (numeriGenerati < 5) {
-        numeroGenerato = Math.floor(Math.random() * 100 + 1)
-        arrayNumeri.push(numeroGenerato)
-        numeriGenerati++
+    bottoneStart.disabled = true;
+    arrayNumeri.length = 0;
+    numeriGenerati = 0;
+    while (arrayNumeri.length < 5) {
+        let numeroGenerato = Math.floor(Math.random() * 100 + 1);
+        if (!arrayNumeri.includes(numeroGenerato)) {
+            arrayNumeri.push(numeroGenerato);
+        }
     }
-    console.log(arrayNumeri)
-    numeroUno.innerHTML = arrayNumeri[0]
-    numeroDue.innerHTML = arrayNumeri[1]
-    numeroTre.innerHTML = arrayNumeri[2]
-    numeroQuattro.innerHTML = arrayNumeri[3]
-    numeroCinque.innerHTML = arrayNumeri[4]
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].innerHTML = arrayNumeri[i];
+    }
+
+    // console.log(arrayNumeri)
+    // numeroUno.innerHTML = arrayNumeri[0]
+    // numeroDue.innerHTML = arrayNumeri[1]
+    // numeroTre.innerHTML = arrayNumeri[2]
+    // numeroQuattro.innerHTML = arrayNumeri[3]
+    // numeroCinque.innerHTML = arrayNumeri[4]
 
     let inputNumerici = document.querySelectorAll('.numeri input[type="number"]');
     inputNumerici.forEach(input => input.disabled = true);
     numeriGenerati = 0
     // arrayNumeri = []
     setTimeout(() => {
-        numeroUno.innerHTML = "";
-        numeroDue.innerHTML = "";
-        numeroTre.innerHTML = "";
-        numeroQuattro.innerHTML = "";
-        numeroCinque.innerHTML = "";
+        // numeroUno.innerHTML = "";
+        // numeroDue.innerHTML = "";
+        // numeroTre.innerHTML = "";
+        // numeroQuattro.innerHTML = "";
+        // numeroCinque.innerHTML = "";
+
+        for (let i = 0; i < numbers.length; i++) {
+            numbers[i].innerHTML = "";
+        }
+
         inputNumerici.forEach(input => input.disabled = false);
         // bottoneStart.disabled = false
         bottoneSubmit.disabled = false
-    }, 30000);
+    }, 3000);
     return arrayNumeri;
 }
 
@@ -64,4 +79,5 @@ function confrontoNumeri() {
     bottoneStart.disabled = false;
     bottoneSubmit.disabled = true;
     arrayNumeri.length = 0;
+    inputNumerici.forEach(input => input.value = '');
 }
